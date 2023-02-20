0719c2c43223d761c88decd769c0bdecdd897726
anshikadixit09 <anshika.dixit@dianapps.com>
2023-02-20 16:20:52 +0530

    change the maptype

--- lib/map.dart
+++ lib/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,13 +11,25 @@ class SimpleMap extends StatefulWidget {
 }
 
 class _SimpleMapState extends State<SimpleMap> {
-
   static const LatLng _kMapCenter =
       LatLng(26.84976407763883, 75.80064458465962);
 
   static const CameraPosition _kInitialPosition =
       CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
 
+  final Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  MapType _defaultMapType = MapType.normal;
+
+  void _changeMapType() {
+    setState(() {
+          _defaultMapType = _defaultMapType == MapType.normal ? MapType.satellite : MapType.normal;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +37,26 @@ class _SimpleMapState extends State<SimpleMap> {
       appBar: AppBar(title: const Text('Google Maps Demo')),
       body: Stack(
         children: [
-          const GoogleMap(  
+          GoogleMap(
+           myLocationEnabled: true,
+            mapType: _defaultMapType,
+            onMapCreated: _onMapCreated,
             initialCameraPosition: _kInitialPosition,
           ),
+          Container(
+            margin: const EdgeInsets.only(top: 80, right: 10),
+            alignment: Alignment.topRight,
+            child: Column(children: [
+              FloatingActionButton(
+                elevation: 5,
+                backgroundColor: Colors.teal[200],
+                onPressed: _changeMapType,
+                
+                child: const Icon(Icons.layers),),
+                const SizedBox(height: 16.0,),
+            ]),
+          ),
+
         ],
       ),
     );
